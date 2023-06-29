9c60543fa79930c1a8c64244e0745f2f001318cc
Yusuf Ayyub <yayyub85@gmail.com>
2023-06-29 21:26:57 +0100

    completed hero component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { navLinks } from "../constants";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
-    <nav className="w-full flex py6 justify-between items-center navbar">
+    <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
@@ -25,23 +25,22 @@ const Navbar = () => {
         <img 
           src={toggle ? close : menu} 
           alt="menu"
-          className="w-[28] h-[28] object-contain"
+          className="w-[28px] h-[28px] object-contain"
           onClick={() => setToggle((prev) => !prev)}
           />
-          <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-            <ul className="list-none flex flex-col justify-end items-center flex-1">
-          {navLinks.map((nav, index) => (
-            <li
-              key={nav.id}
-              className={`font-poppins font-normal cursor-pointer text-[16px] ${
-                index === navLinks.length - 1 ? `mr-0` : `mb-4`
-              } text-white`}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
-        </ul>
-          </div>
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? `mb-0` : `mb-4`
+                } text-white`}>
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
